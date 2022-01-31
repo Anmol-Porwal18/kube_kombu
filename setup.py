95b1fb59e4329d3a39ddd5194a031dddebdbe8cc
Anmol Porwal <anmolporwal@ymail.com>
2022-01-31 14:23:00 +0530

    Bump version to 1.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="kube_kombu",
-    version="v1.0.4",
+    version="v1.0.5",
     install_requires=[
         "kombu==5.2.3",
     ],
