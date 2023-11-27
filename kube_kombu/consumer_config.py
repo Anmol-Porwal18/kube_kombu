0033a26eab838fa077dc6edd6390d0e4525c22a2
Sahil Mittal <sahilm@agrevolution.in>
2023-11-27 17:32:21 +0530

    Add support for multiple bindings

--- kube_kombu/consumer_config.py
+++ kube_kombu/consumer_config.py
@@ -1,7 +1,22 @@
 class ConsumerConfig:
-    def __init__(self, url, exchange, exchange_type, routing_key, queue_name):
+    def __init__(
+        self,
+        url,
+        exchange=None,
+        exchange_type=None,
+        routing_key=None,
+        queue_name=None,
+        bindings=[],
+    ):
+        if not queue_name:
+            raise ValueError("Consumer without a queue name. Really?")
+        if bindings and (exchange or exchange_type or routing_key):
+            raise ValueError(
+                "Conflicting Input Provided: Either specify bindings or exchange parameters only"
+            )
         self.url = url
         self.exchange = exchange
         self.exchange_type = exchange_type
         self.routing_key = routing_key
         self.queue_name = queue_name
+        self.bindings = bindings
